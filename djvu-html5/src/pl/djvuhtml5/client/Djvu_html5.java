347ec27537c6e664e6ed74b5114e0a0f4780b776
Mateusz Matela <mateusz.matela@gmail.com>
2015-04-23 21:06:45 +0200

    general layout

--- djvu-html5/src/pl/djvuhtml5/client/Djvu_html5.java
+++ djvu-html5/src/pl/djvuhtml5/client/Djvu_html5.java
@@ -6,8 +6,17 @@ import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.ImageData;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.lizardtech.djvu.CachedInputStream;
 import com.lizardtech.djvu.DjVmDir;
 import com.lizardtech.djvu.DjVuPage;
@@ -23,26 +32,17 @@ public class Djvu_html5 implements EntryPoint {
 
 	private String url;
 	private Document document;
+	private Canvas canvas;
 	private Context2d drawingContext;
-
 	/**
 	 * This is the entry point method.
 	 */
 	@Override
 	public void onModuleLoad() {
 
-
-		Canvas canvas = Canvas.createIfSupported();
-		if (canvas == null) {
-			// TODO
-			throw new RuntimeException("Canvas not supported!");
-		}
-		canvas.setSize("1000px", "1000px");
-		canvas.setCoordinateSpaceWidth(1000);
-		canvas.setCoordinateSpaceHeight(1000);
-		drawingContext = canvas.getContext2d();
 		RootPanel container = RootPanel.get("djvuContainer");
-		container.add(canvas);
+		container.add(prepareToolBar());
+		container.add(prepareCanvas());
 
 		DjvuContext.init(drawingContext);
 
@@ -63,6 +63,62 @@ public class Djvu_html5 implements EntryPoint {
 				parseDocument();
 			}
 		});
+	}
+
+	private Widget prepareCanvas() {
+		canvas = Canvas.createIfSupported();
+		if (canvas == null) {
+			// TODO
+			throw new RuntimeException("Canvas not supported!");
+		}
+		drawingContext = canvas.getContext2d();
+
+		final SimplePanel panel = new SimplePanel(canvas);
+		panel.setStyleName("content");
+
+		Window.addResizeHandler(new ResizeHandler() {
+			
+			@Override
+			public void onResize(ResizeEvent event) {
+				resizeCanvas();
+			}
+		});
+		Scheduler.get().scheduleFinally(new Scheduler.ScheduledCommand() {
+			
+			@Override
+			public void execute() {
+				resizeCanvas();
+			}
+		});
+		return panel;
+	}
+
+	private void resizeCanvas() {
+		Widget panel = canvas.getParent();
+		int width = panel.getOffsetWidth();
+		int height = panel.getOffsetHeight();
+		canvas.setWidth(width + "px");
+		canvas.setCoordinateSpaceWidth(width);
+		canvas.setHeight(height + "px");
+		canvas.setCoordinateSpaceHeight(height);
+	}
+
+	private Widget prepareToolBar() {
+		HorizontalPanel toolBar = new HorizontalPanel();
+		toolBar.setStyleName("toolbar");
+
+		Button button = new Button();
+		button.setStyleName("toolbarSquareButton");
+		button.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				parsePages();
+			}
+		});
+		toolBar.add(button);
+
+		return toolBar;
 	}
 
 	private void parseDocument() {
@@ -107,14 +163,12 @@ public class Djvu_html5 implements EntryPoint {
 //		}
 		try {
 			DjVuPage page = document.getPage(0);
-			int w = 1000, h = 1000;
+			int w = Math.min(canvas.getCoordinateSpaceWidth(), page.getInfo().width);
+			int h = Math.min(canvas.getCoordinateSpaceHeight(), page.getInfo().height);
 			GRect segment = new GRect(0, page.getInfo().height - h, w, h);
-			long start = System.currentTimeMillis();
 			GMap map = page.getMap(segment, 1, null);
 			byte[] data = map.getData();
-			System.out.println("bitmap generation: " + (System.currentTimeMillis() - start));
-			start = System.currentTimeMillis();
-			ImageData imageData = drawingContext.createImageData(1000, 1000);
+			ImageData imageData = drawingContext.createImageData(w, h);
 			for (int y = 0; y < h; y++) {
 				for (int x = 0; x < w; x++) {
 					int offset = 3 * ((h - y - 1) * w + x);
@@ -124,7 +178,6 @@ public class Djvu_html5 implements EntryPoint {
 					imageData.setAlphaAt(255, x, y);
 				}
 			}
-			System.out.println("bitmap copying: " + (System.currentTimeMillis() - start));
 			drawingContext.putImageData(imageData, 0, 0);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
